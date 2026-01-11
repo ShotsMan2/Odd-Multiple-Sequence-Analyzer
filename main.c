7bcbedb4bbd3eeab4b2a7a183c7a326a880e86ba
Selim Efe <uysalselimefe@gmail.com>
2026-01-11 13:22:57 +0300

    Implement conditional filtering and reverse output logic
    Added source code that populates an array with integers satisfying mathematical criteria ($x>10, x\%2!=0, x\%3==0$) and prints the buffer in reverse to simulate descending sort.

---
+++ main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+               int array[5],i,index=0,j;
+
+
+    for(i=11;;i++){
+        if(i%2==1){
+            if(i%3==0){
+                array[index]=i;
+
+
+
+                index++;
+            }
+        }
+        if(index==5){
+            break;
+        }
+    }
+    for(j=4;j>=0;j--){
+        printf("%d ",array[j]);
+    }
+
+
+
+    return 0;
+}
