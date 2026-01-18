e374f403091ef2d6a4e389de1b9cdd4aa6ad89cb
Selim Efe <uysalselimefe@gmail.com>
2026-01-18 23:03:19 +0300

    Update main.c

--- main.c
+++ main.c
@@ -3,28 +3,28 @@
 
 int main()
 {
-               int array[5],i,index=0,j;
+    int array[5], i, index = 0, j;
 
-
-    for(i=11;;i++){
-        if(i%2==1){
-            if(i%3==0){
-                array[index]=i;
-
-
+    for (i = 11;; i++)
+    {
+        if (i % 2 == 1)
+        {
+            if (i % 3 == 0)
+            {
+                array[index] = i;
 
                 index++;
             }
         }
-        if(index==5){
+        if (index == 5)
+        {
             break;
         }
     }
-    for(j=4;j>=0;j--){
-        printf("%d ",array[j]);
+    for (j = 4; j >= 0; j--)
+    {
+        printf("%d ", array[j]);
     }
-
-
 
     return 0;
 }
